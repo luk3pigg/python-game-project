bd076facbdc72e6d35aa8717d86f77f113513479
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-08 20:51:17 +0000

    Added word bank, random word selector, and pseudocode comments for initial logic

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  5 17:09:47 2026
+#Hangman Game Project
+import random
 
-@author: luk3p
-"""
+#eventually, create word banks based on different lengths of words and ask user for input on which lenght they want
+
+#create a word bank (start with 6 letters)
+
+word_bank = ['python', 'guitar', 'coffee', 'breeze', 'window', 'galaxy', 'jungle', 'silver', 'orchid', 'poetry']
+
+#select which word from word bank
+
+chosen_index = random.randint(0, len(word_bank) - 1)
+chosen_word = word_bank[chosen_index]
+print(chosen_word)
+
+#main game
+#display current state of word: blanks for unrevaled letters
+
+#limited number of incorrect guesses - typically X amount
+
+#take letter guess from user - make sure hasn't already been guessed, and must be a letter. make it lowercase
+
+#reveal if this letter is part of word or not
+
+#repeat process until either number of gusses used up, or word is guessed  orrectyl
+
+
+#ask if user wnats to repeat the game. 
+
 
