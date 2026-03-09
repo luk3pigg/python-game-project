514a99d29396c4f068051ce109e1a0591e5fb5b6
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-09 13:40:53 +0000

    updated messages depending on how many goes have been taken

--- game_functions.py
+++ game_functions.py
@@ -2,7 +2,7 @@ import random
 
 def start_game():
     '''starts the game based on user input'''
-    start_game_response = input("Welcome! Do you want to play hangman?\nYES: enter y\nNO: enter n\n").lower().strip()
+    start_game_response = input("Welcome! Are you ready to play hangman?\nYES: enter y\nNO: enter n\n\n").lower().strip()
     while True:
         if start_game_response == 'y':
             return True
@@ -10,12 +10,14 @@ def start_game():
             print("\nOh, never mind. Have a nice day!")
             return False
         else:
-            start_game_response = input("\nUnfortunately, that's an an invalid input. Please try again.\nYES: enter y\nNO: enter n\n").lower().strip()
+            start_game_response = input("\nUnfortunately, that's an an invalid input. Please try again.\nYES: enter y\nNO: enter n\n\n").lower().strip()
+
 
 def select_word(word_bank):
     '''Selects a word at random from the word bank'''
     chosen_index = random.randint(0, len(word_bank) - 1)
     return word_bank[chosen_index]
+
 
 def letter_validation(total_guesses, guessed_letters):
     '''Inputs a letter guess from user and ensures both a.) it's a letter b.) it hasn't been guessed already.'''
@@ -25,7 +27,8 @@ def letter_validation(total_guesses, guessed_letters):
             total_guesses += 1
             return letter_guess, total_guesses
         else:
-            print("\n\n\nThat guess was invalid.")
+            print("\n\n\nThat guess was invalid. Ensure the guess is a single letter that hasn't already been guessed!")
+
 
 def guess_result(guessed_letters, chosen_word, display_word, letter_guess, lives, correct_guesses, game_won):
     '''Adds guess to guessed letters list, determines if guess is correct, and updates lives accordingly '''
@@ -37,6 +40,8 @@ def guess_result(guessed_letters, chosen_word, display_word, letter_guess, lives
                 ocurrences += 1
                 display_word[i] = letter_guess
         correct_guesses += ocurrences
+        print("\n\n")
+        print(*display_word)
         if correct_guesses == len(chosen_word):
             game_won = True
         else:
@@ -47,9 +52,10 @@ def guess_result(guessed_letters, chosen_word, display_word, letter_guess, lives
         lives -= 1
     return lives, correct_guesses, game_won
 
+
 def restart_game():
     '''restarts the game based on user input'''
-    restart_game_response = input("Do you want to play the game again?\n YES: enter y\n NO: enter n\n").lower().strip()
+    restart_game_response = input("Do you want to play another game?\n YES: enter y\n NO: enter n\n\n").lower().strip()
     while True:
         if restart_game_response == 'y':
             return True
@@ -57,7 +63,7 @@ def restart_game():
             print("\nOh, never mind. Have a nice day!")
             return False
         else:
-            restart_game_response = input("\nUnfortunately, that's an an invalid input. Please try again.\n YES: enter y\n NO: enter n\n").lower().strip()
+            restart_game_response = input("\nUnfortunately, that's an an invalid input. Please try again.\n YES: enter y\n NO: enter n\n\n").lower().strip()
             
-            
+
 
--- main.py
+++ main.py
@@ -19,23 +19,66 @@ total_session_games = 0
 game_active = gf.start_game()
 while game_active:
     chosen_word = gf.select_word(word_bank = word_bank)
-    #would you like to know the rules of the game yes: y n: n
-    #Choose your settings: lives, word length, timer?
     lives = 6
+    
+    #Initialisation
+    
     correct_guesses = 0
     guessed_letters = []
     display_word = ['_'] * len(chosen_word)
     total_guesses = 0
     game_won = False
+    
+    
+    
+    #if total guesses = 0
+    
+    #explain rules?
+    #choose settings
+    
+    
+   
+        # explain_rules = input("Would you like to read the rules first?\nYES: enter y\nNO: enter n\n").lower().strip()
+        # while True:
+        #     if explain_rules == 'y':
+        #         return True
+        #     elif explain rules == 'n':
+        #         print("\nOh, never mind. Have a nice day!")
+        #         return False
+        #     else:
+        #         start_game_response = input("\nUnfortunately, that's an an invalid input. Please try again.\nYES: enter y\nNO: enter n\n").lower().strip()
+        
+
+
+
+
+         
+        
+        
+    #would you like to know the rules of the game yes: y n: n
+    #Choose your settings: lives, word length, timer?
+    
+         
+    
+    
+    
     #main game 
     while lives > 0 and not game_won: 
-        print(f"\n\n\nYou currently have {lives} lives.")
-        print(f"You currently have {correct_guesses} correct guesses.")
-        if total_guesses > 0:
-            print("You have guessed the following letters:")
+        if total_guesses == 0:
+            print("\nThe timer is on!")
+            print("\nThis is your first guess!")
+            print(f"\nYou currently have {lives} lives.")
+            print("\nThis is the secret word:")
+            print(*display_word)
+        else:
+            print("\n20 seconds have gone so far!")
+            print(f"\nThis is guess number {total_guesses + 1}.")
+            print(f"\nYou currently have {lives} lives.")
+            print(f"\nYou currently have {correct_guesses} correct guesses.")
+            print("\nYou have guessed the following letters:")
             print(*guessed_letters)
-        print("Here's what you know so far:")
-        print(*display_word)
+            print("\nHere's what you know so far:")
+            print(*display_word)
         
         letter_guess, total_guesses = gf.letter_validation(total_guesses=total_guesses, guessed_letters=guessed_letters)
         lives, correct_guesses, game_won = gf.guess_result(guessed_letters, chosen_word, display_word, letter_guess=letter_guess, lives=lives, correct_guesses=correct_guesses, game_won=game_won)
@@ -43,7 +86,7 @@ while game_active:
         
     
     if game_won:
-        print(f"You won! The secret word was {chosen_word}!")
+        print(f"\n\nYou won! The secret word was {chosen_word}!")
     else:
         print(f"Oh no! You have run out of lives! The correct word was {chosen_word}")
     #stats
