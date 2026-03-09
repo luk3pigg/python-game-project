7e94887ed18f3b386d7b2531d2427975ef6a4a47
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-09 12:34:53 +0000

    Restructured game into modules

---
+++ game_functions.py
@@ -0,0 +1,58 @@
+import random
+
+def start_game():
+    '''starts the game based on user input'''
+    while True:
+        start_game_response = input("Do you want to play hangman?\n YES: enter y\n NO: enter n\n").lower().strip()
+        if start_game_response == 'y':
+            return True
+        elif start_game_response == 'n':
+            return False
+        else:
+            print("Unfortunately, that's an an invalid input.")
+
+def select_word(word_bank):
+    '''Selects a word at random from the word bank'''
+    chosen_index = random.randint(0, len(word_bank) - 1)
+    return word_bank[chosen_index]
+
+def letter_validation(total_guesses, guessed_letters):
+    '''Inputs a letter guess from user and ensures both a.) it's a letter b.) it hasn't been guessed already.'''
+    while True:
+        letter_guess = input("\n\n\nPlease enter a letter guess: ")
+        if letter_guess.isalpha() and len(letter_guess) == 1 and letter_guess not in guessed_letters:
+            total_guesses += 1
+            return letter_guess, total_guesses
+        else:
+            print("\n\n\nThat guess was invalid.")
+
+def guess_result(guessed_letters, chosen_word, display_word, letter_guess, lives, correct_guesses, game_won):
+    '''Adds guess to guessed letters list, determines if guess is correct, and updates lives accordingly '''
+    guessed_letters.append(letter_guess)
+    if letter_guess in chosen_word:
+        ocurrences = 0
+        for i, letter in enumerate(chosen_word):
+            if letter == letter_guess:
+                ocurrences += 1
+                display_word[i] = letter_guess
+        correct_guesses += ocurrences
+        if correct_guesses == len(chosen_word):
+            game_won = True
+        else:
+            print("\n\n\nCorrect guess!")
+            print(f"Your guess appears in the secret word {ocurrences} times!")
+    else:
+        print("\n\n\nIncorrect guess...Unlucky!")
+        lives -= 1
+    return lives, correct_guesses, game_won
+
+def restart_game():
+    '''restarts the game based on user input'''
+    while True:
+        restart_game_response = input("Do you want to play the game again?\n YES: enter y\n NO: enter n\n").lower().strip()
+        if restart_game_response == 'y':
+            return True
+        elif restart_game_response == 'n':
+            return False
+        else:
+            print("Unfortunately, that's an an invalid input.")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#Hangman Game Project
-import random
+import game_functions as gf
 
 #additional ideas to focus on later:
 #eventually, create word banks based on different lengths of words and ask user for input on which lenght they want
@@ -16,68 +15,13 @@ import random
 
 word_bank = ['python', 'guitar', 'coffee', 'breeze', 'window', 'galaxy', 'jungle', 'silver', 'orchid', 'poetry']
 
-def start_game():
-    '''starts the game based on user input'''
-    while True:
-        start_game_response = input("Do you want to play hangman?\n YES: enter y\n NO: enter n\n").lower().strip()
-        if start_game_response == 'y':
-            return True
-        elif start_game_response == 'n':
-            return False
-        else:
-            print("Unfortunately, that's an an invalid input.")
 
-def select_word():
-    '''Selects a word at random from the word bank'''
-    chosen_index = random.randint(0, len(word_bank) - 1)
-    return word_bank[chosen_index]
-
-def letter_validation(total_guesses):
-    '''Inputs a letter guess from user and ensures both a.) it's a letter b.) it hasn't been guessed already.'''
-    while True:
-        letter_guess = input("\n\n\nPlease enter a letter guess: ")
-        if letter_guess.isalpha() and len(letter_guess) == 1 and letter_guess not in guessed_letters:
-            total_guesses += 1
-            return letter_guess, total_guesses
-        else:
-            print("\n\n\nThat guess was invalid.")
-
-def guess_result(letter_guess, lives, correct_guesses, game_won):
-    '''Adds guess to guessed letters list, determines if guess is correct, and updates lives accordingly '''
-    guessed_letters.append(letter_guess)
-    if letter_guess in chosen_word:
-        ocurrences = 0
-        for i, letter in enumerate(chosen_word):
-            if letter == letter_guess:
-                ocurrences += 1
-                display_word[i] = letter_guess
-        correct_guesses += ocurrences
-        if correct_guesses == len(chosen_word):
-            game_won = True
-        else:
-            print("\n\n\nCorrect guess!")
-            print(f"Your guess appears in the secret word {ocurrences} times!")
-    else:
-        print("\n\n\nIncorrect guess...Unlucky!")
-        lives -= 1
-    return lives, correct_guesses, game_won
-
-def restart_game():
-    '''restarts the game based on user input'''
-    while True:
-        restart_game_response = input("Do you want to play the game again?\n YES: enter y\n NO: enter n\n").lower().strip()
-        if restart_game_response == 'y':
-            return True
-        elif restart_game_response == 'n':
-            return False
-        else:
-            print("Unfortunately, that's an an invalid input.")
 
 #starting the game
 total_session_games = 0
-game_active = start_game()
+game_active = gf.start_game()
 while game_active:
-    chosen_word = select_word()
+    chosen_word = gf.select_word(word_bank = word_bank)
     #would you like to know the rules of the game yes: y n: n
     #Choose your settings: lives, word length, timer?
     lives = 6
@@ -96,8 +40,8 @@ while game_active:
         print("Here's what you know so far:")
         print(*display_word)
         
-        letter_guess, total_guesses = letter_validation(total_guesses)
-        lives, correct_guesses, game_won = guess_result(letter_guess=letter_guess, lives=lives, correct_guesses=correct_guesses, game_won=game_won)
+        letter_guess, total_guesses = gf.letter_validation(total_guesses=total_guesses, guessed_letters=guessed_letters)
+        lives, correct_guesses, game_won = gf.guess_result(guessed_letters, chosen_word, display_word, letter_guess=letter_guess, lives=lives, correct_guesses=correct_guesses, game_won=game_won)
     
         
     
@@ -109,7 +53,7 @@ while game_active:
     total_session_games += 1
     print(f"Total games you have played in this session: {total_session_games}")
     #restart game option
-    game_active = restart_game()
+    game_active = gf.restart_game()
     
 
 
